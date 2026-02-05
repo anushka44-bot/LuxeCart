3869d02aa0b3e6f044bd4432dd17a937f320acd6
Anushka Bag <bag82262@gmail.com>
2026-02-05 20:53:57 +0530

    Home pg complete

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text:sm ">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ut
+            consectetur nulla necessitatibus possimus reprehenderit alias
+            placeat adipisci ea minima.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+918974689540</li>
+            <li>contact@luxecart.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-xm text-center">
+          Copyright 2026@ LuxeCart.com - All Right Reserved
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
