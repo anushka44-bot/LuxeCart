4e32e2874f53918ef9f24bd55d1560b2570f14cc
Anushka Bag <bag82262@gmail.com>
2026-02-19 19:34:06 +0530

    Products pg update

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -30,6 +30,12 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) =>
@@ -65,7 +71,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter();
-  }, [category, SubCategory]);
+  }, [category, SubCategory, search, showSearch]);
 
   useEffect(() => {
     sortProduct();
@@ -178,7 +184,7 @@ const Collection = () => {
             <ProductItem
               key={index}
               name={item.name}
-              id={item.id}
+              id={item._id}
               price={item.price}
               image={item.image}
             />
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Product = () => {
-  return <div></div>;
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productsData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productsData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* product img */}
+      <div className="flex gap-4 sm:gap-6 flex-col sm:flex-row items-start">
+        <div className="flex sm:flex-col flex-shrink-0 sm:w-[90px] w-full gap-3 overflow-x-auto sm:overflow-y-auto">
+          {productsData.image.map((item, index) => (
+            <img
+              onClick={() => setImage(item)}
+              src={item}
+              key={index}
+              className="w-20 sm:w-full cursor-pointer"
+              alt=""
+            />
+          ))}
+        </div>
+        <div className="flex-1 max-w-[500px]">
+          <img
+            onClick={() => setImage(item)}
+            className="w-full h-auto"
+            src={image}
+            alt=""
+          />
+        </div>
+
+        {/*---------product info---------*/}
+        <div className="sm:w-[40%] w-full">
+          <h1 className="font-medium text-2xl mt-2">{productsData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productsData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productsData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productsData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${item === size ? "border-orange-500" : ""}`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on Delivery is available</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+      {/*------ Description & Review section -----*/}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
