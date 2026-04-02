ed12bcb4c4fc017eec7f0704dfe079107df5f22e
Anushka Bag <bag82262@gmail.com>
2026-04-02 11:16:18 +0530

    Registration and Login controls

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB=async()=>{
-  mongoose.connection.on('connected',()=>{
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => {
     console.log('DB Connected');
-  })
-  await mongoose.connect(`${process.env.MONGODB_URL}/e-commerce`)
-}
+  });
 
+  await mongoose.connect(process.env.MONGODB_URL);
+};
 
 export default connectDB;
