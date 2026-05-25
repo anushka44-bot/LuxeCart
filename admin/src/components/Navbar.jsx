1d7244734425445eb9b1f5ab87bf3761d737cc4d
Anushka Bag <bag82262@gmail.com>
2026-05-25 11:24:04 +0530

    Admin Panel updates

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { assets } from "../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img className="w-60 sm:w-52" src={assets.logo} alt="" />
 
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
         Logout
       </button>
     </div>
