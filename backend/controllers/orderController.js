853199d0e3af54d848cd8b4ce8ad8a287c7e2c8d
Anushka Bag <bag82262@gmail.com>
2026-06-15 10:28:05 +0530

    Orders page update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -59,7 +59,17 @@ const allOrders=async(req,res)=>{
 
 //All orders data for frontend
 const userOrders=async(req,res)=>{
-  
+  try {
+    const{userId}=req.body
+    const orders=await orderModel.find({userId})
+    res.json({success:true,orders})
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
 
 //update order status for admin panel
