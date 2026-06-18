44631cd97c33dbe5a98c4dfee711a84c3ae0817e
Anushka Bag <bag82262@gmail.com>
2026-06-18 20:39:29 +0530

    Stripe verification update

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Verify = () => {
-  return <div></div>;
+  const { token, setCartItems, backendUrl } = useContext(ShopContext);
+
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+
+  const verifyPayment = async () => {
+    try {
+      if (!token) return;
+
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyStripe",
+        {
+          success: success === "true",
+          orderId,
+        },
+        {
+          headers: { token },
+        },
+      );
+
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/cart");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, [token]);
+
+  return (
+    <div className="min-h-[60vh] flex items-center justify-center">
+      <p>Verifying Payment...</p>
+    </div>
+  );
 };
 
 export default Verify;
